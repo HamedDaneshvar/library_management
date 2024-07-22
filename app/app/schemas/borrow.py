7c924ae408d6a1615549805a7f1aeb5f4d60c16d
Hamed Daneshvar <hmddaneshvar@gmail.com>
2024-07-23 00:43:58 +0330

    feat: Add Borrow, BorrowActivityLog, UserPenalty schema

--- app/app/schemas/borrow.py
+++ app/app/schemas/borrow.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 
@@ -10,4 +11,51 @@ class StatusCreate(Status):
 
 
 class StatusUpdate(Status):
+    pass
+
+
+class Borrow(BaseModel):
+    book_id: int
+    superuser_id: int
+    user_id: int
+    start_date: datetime
+    max_delivery_date: datetime
+    delivery_date: datetime
+    borrow_price: float
+    borrow_penalty_price: float
+    total_price: float
+
+
+class BorrowCreate(Borrow):
+    pass
+
+
+class Borrowupdate(Borrow):
+    pass
+
+
+class BorrowActivityLog(BaseModel):
+    borrow_id: int
+    status_id: int
+
+
+class BorrowActivityLogCreate(BorrowActivityLog):
+    pass
+
+
+class BorrowActivityLogUpdate(BorrowActivityLog):
+    pass
+
+
+class UserPenalty(BaseModel):
+    user_id: int
+    borrow_id: int 
+    borrow_penalty_day: int
+
+
+class UserPenaltyCreate(UserPenalty):
+    pass
+
+
+class UserPenaltyUpdate(UserPenalty):
     pass
